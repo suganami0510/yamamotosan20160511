080570d4bf4c56c6d89471d1712ab3995cf00e81
hJosuke <ryosuke.suganami@gmail.com>
2016-05-11 15:22:31 +0900

    create User_show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/show'
-
-  get 'users/edit'
-
+  get 'users/show' => 'users#show'
+  get 'users/edit' => 'users#edit'
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!(name:"Example User",
+             profile:"example@railstutorial.org",
+             birthday:"2001-02-03")
